2b26881d6bcef3c51b7801a287d6a03cab86b3c1
Su ziyang <suziyang@sjtu.edu.cn>
2023-10-11 19:22:44 +0800

    add files

---
+++ Lecture 5/Tutorial.md
@@ -0,0 +1,3 @@
+1. Pull docker image ``docker pull poxinz/demo_image:1.2``
+2. Download code files.
+3. Put ``lecture_5.ipynb`` and ``CIFAR10`` in the same folder.
---
+++ Lecture 5/lecture5test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import heapq
+from scipy import spatial
+from scipy.spatial import distance
+from tqdm.notebook import tqdm, trange
+import copy
+import random
+import cv2
